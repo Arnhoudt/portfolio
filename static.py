3a7685bdc544db441a2713379c8f18230249bce1
Anton Kindt <anton.kindt@ugent.be>
2022-02-17 11:40:53 +0100

    Added minify to the static site generator

--- static.py
+++ static.py
@@ -3,6 +3,9 @@ from app import app
 
 from flask_frozen import Freezer
 
+from flask_minify import Minify
+
+Minify(app=app, html=True, js=True, cssless=True)
 
 freezer = Freezer(app, with_static_files=True, log_url_for=False)
 
