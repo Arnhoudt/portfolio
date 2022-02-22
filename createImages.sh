6a7989eeff00edc30a684d1160a012962c945bb0
Anton Kindt <anton.kindt@ugent.be>
2022-02-22 16:50:25 +0100

    createImages creates a duplicate of every svg in the build directory

--- createImages.sh
+++ createImages.sh
@@ -8,7 +8,7 @@ if [ -d "./static/build/img" ]; then
 fi
 
 
-# for each file in static/src/img
+# for each png in static/src/img
 for f in $(find ./static/src/img -type f -regex ".*\.png"); do
     # do some stuff here with "$f"
     # remember to quote it or spaces may misbehave
@@ -19,4 +19,15 @@ for f in $(find ./static/src/img -type f -regex ".*\.png"); do
         mkdir -p "./static/build/img/$directory"
         convert -colorspace RGB -resize ${size} -crop ${size}x$(expr $size / 16 \* 9)+0+0 $f "./static/build/img/$directory/${size}_$basename"
     done
+done
+
+# for each file in static/src/img
+for f in $(find ./static/src/img -type f -regex ".*\.svg"); do
+    # do some stuff here with "$f"
+    # remember to quote it or spaces may misbehave
+    name=$(echo $f | sed -e 's/.*\/img\///g')
+    basename=$(basename $f)
+    directory=$(echo $name | sed -e 's/\/*[^\/]*$//g')
+    mkdir -p "./static/build/img/$directory"
+    convert -colorspace RGB $f "./static/build/img/$directory/$basename"
 done
