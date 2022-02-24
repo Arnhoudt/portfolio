dcd2150edd1b4e2b8886a619286e5940063db797
Anton Kindt <anton.kindt@ugent.be>
2022-02-24 16:18:34 +0100

    added a gradiend over the projectImage to make it more distinct

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
@@ -12,5 +14,15 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.bg-overlay': {
+          'background': 'radial-gradient(circle, rgba(0,0,0,0) 0%, rgba(0,0,0,0.1) 200%);',
+          'background-position': 'center',
+          'background-size': 'cover',
+        },
+      });
+    }),
+  ],
 }
